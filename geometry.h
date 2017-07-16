4ae7c745ca37a98287b91eed0fe48aa303c06317
Andrew Reece <azmreece@gmail.com>
2017-07-16 16:09:38 +0100

    consolidate generic geometry functions

---
+++ geometry.h
@@ -0,0 +1,274 @@
+#ifndef GEOMETRY_H
+#include <maths.h>
+
+// TODO: Should these be f32?
+internal inline v2
+Proj(v2 V, v2 W)
+{
+	// NOTE: does not assume normalized
+	v2 Result = V2Mult((Dot(V, W)/LenSq(W)), W);
+	return Result;
+}
+
+internal inline v2
+ProjN(v2 V, v2 W)
+{
+	// NOTE: assumes normalized(?)
+	v2 Result = V2Mult(Dot(V, W), W);
+	return Result;
+}
+
+/// AB = B - A
+internal inline v2
+ClosestPtOnSegment(v2 P, v2 a, v2 ab)
+{
+	BEGIN_TIMED_BLOCK;
+	// Project P onto ab, computing parameterized position d(t) = a + t*(b - a)
+	// TODO: Dot(ab, ab) better as Len(ab)?
+	f32 t = Dot(V2Sub(P, a), ab) / Dot(ab, ab);
+	// If outside segment, closest point is an endpoint
+	t = Clamp(t, 0.f, 1.f);
+	// TODO: is t wanted as well?
+	v2 Result = V2Add(a, V2Mult(t, ab));
+	END_TIMED_BLOCK;
+	return Result;
+}
+
+/// ab = b - a
+internal inline v2
+ClosestPtOnSegmentDeferred(v2 P, v2 a, v2 ab)
+{
+	BEGIN_TIMED_BLOCK;
+	// Project P onto AB, computing parameterized position d(t) = a + t*(b - a)
+	f32 t = Dot(V2Sub(P, a), ab);
+	v2 Result;
+	// If outside segment on (left), closest point is left endpoint
+	if(t <= 0.f)
+	{
+		// t = 0.f;
+		Result = a;
+	}
+
+	else
+	{
+		f32 Denominator = Dot(ab, ab); // Always nonnegative since denom = ||AB||^2
+		if(t >= Denominator)
+		{
+			// t = 1.f;
+			Result = V2Add(a, ab);
+		}
+
+		else
+		{
+			t = t / Denominator;
+			// TODO: is t wanted as well?
+			Result = V2Add(a, V2Mult(t, ab));
+		}
+	}
+	END_TIMED_BLOCK;
+	return Result;
+}
+
+internal inline f32
+DistSqPtSegment(v2 P, v2 A, v2 B)
+{
+	BEGIN_TIMED_BLOCK;
+	f32 Result;
+	v2 ab = V2Sub(B, A), ap = V2Sub(P, A), bp = V2Sub(P, B);
+	f32 E = Dot(ap, ab);
+	if(E <= 0.f)
+	{
+		// NOTE: P to 'left' of A
+		Result = Dot(ap, ap);
+	}
+
+	else
+	{
+		f32 F = Dot(ab, ab);
+		if(E >=F)
+		{
+		// NOTE: P to 'right' of B
+			Result = Dot(bp, bp);
+		}
+
+		else
+		{
+			Result = Dot(ap, ap) - E*E / F;
+		}
+	}
+	END_TIMED_BLOCK;
+	return Result;
+}
+
+// Returns 2 times the signed triangle area. The result is positive if
+// abc is ccw, negative if abc is cw, zero if abc is degenerate.
+internal f32
+Signed2DTriAreaDoubled(v2 A, v2 B, v2 C)
+{
+	f32 Result = (A.X - C.X) * (B.Y - C.Y) - (A.Y - C.Y) * (B.X - C.X); 
+	return Result;
+}
+
+// NOTE: From Ericson - Real Time Collision (p153)
+// TODO: compare dot/cross product based intersection
+internal b32
+IntersectSegmentsWinding(v2 A, v2 B, v2 C, v2 D, v2 *Out)
+{
+	BEGIN_TIMED_BLOCK;
+	b32 Result = 0;
+	f32 ABC = Signed2DTriAreaDoubled(A, B, C);
+	f32 ABD = Signed2DTriAreaDoubled(A, B, D);
+	// NOTE: Different sides -> different signs
+	if(ABC * ABD < 0.f)
+	{
+		f32 CDA = Signed2DTriAreaDoubled(C, D, A);
+		// NOTE: Since area is constant ABD - ABC = CDA - CDB, or CDB = CDA + ABC - ABD
+		// f32 CDB = Signed2DTriArea(c, d, b); // Must have opposite sign of a3
+		f32 CDB = CDA + ABC - ABD;
+
+		if(CDA * CDB < 0.f)
+		{
+			// Segments intersect. Find intersection point along L(t) = a + t * (b - a).
+			// Given height h1 of an over cd and height h2 of b over cd,
+			// t = h1 / (h1 - h2) = (b*h1/2) / (b*h1/2 - b*h2/2) = CDA / (CDA - CDB),
+			// where b (the base of the triangles cda and cdb, i.e., the length of cd) cancels out.
+			Result = 1;
+			// TODO: maybe return t?
+			f32 t = CDA / (CDA - CDB);
+			*Out = V2Add(A, V2Mult(t, V2Sub(B, A)));
+		}
+	}
+	END_TIMED_BLOCK;
+	return Result;
+}
+
+/// expects d to be normalised
+internal uint
+IntersectLineCircleForT(v2 P, v2 d, v2 Focus, f32 Radius, f32 *t1, f32 *t2)
+{
+	uint Result = 0;
+	// Distance from point on line to circle centre == radius:
+	// 		(P + td - C) DOT (P + td - C) == radius^2
+	// m=P-C
+	v2 m = V2Sub(P, Focus);
+	// Simplifies to t^2 + 2(m DOT d)t + (m DOT m) - r^2 == 0
+	// Solving quadratic equation  -b +/- sqrt(b^2 - c)
+	f32 b = Dot(m, d);
+	f32 Dotmm = Dot(m, m);
+	f32 c = Dotmm - Radius*Radius;
+	f32 Discriminant = b*b - c;
+
+	if(Discriminant < 0.f) return Result;
+	else if(Discriminant == 0.f)
+	{
+		Result = 1;
+		*t1 = -b;
+	}
+
+	else // if(Discriminant > 0.f)
+	{
+		// TODO: check if t makes ray/segment start/end inside circle
+		Result = 2;
+		f32 RootDisc = QSqrt(Discriminant);
+		*t1 = -b - RootDisc;
+		*t2 = -b + RootDisc;
+	}
+
+	return Result;
+}
+
+internal uint
+IntersectLineCircle(v2 P, v2 Dir, v2 Focus, f32 Radius, v2 *Intersection1, v2 *Intersection2)
+{
+	f32 t1, t2;
+	v2 d = Norm(Dir);
+	uint Result = IntersectLineCircleForT(P, d, Focus, Radius, &t1, &t2);
+
+	if(Result)
+	{
+		*Intersection1 = V2Add(P, V2Mult(t1, d));
+		//if(Result == 2)
+			*Intersection2 = V2Add(P, V2Mult(t2, d));
+	}
+	return Result;
+}
+
+internal uint
+IntersectRayCircle(v2 P, v2 Dir, v2 Focus, f32 Radius, v2 *Intersection1, v2 *Intersection2)
+{
+	f32 t1, t2;
+	v2 d = Norm(Dir);
+	uint Result = IntersectLineCircleForT(P, d, Focus, Radius, &t1, &t2);
+
+	if(Result)
+	{
+		b32 FirstPoint = 1;
+		if(t1 < 0.f) FirstPoint = 0;
+		if(FirstPoint)
+		{
+			*Intersection1 = V2Add(P, V2Mult(t1, d));
+			*Intersection2 = V2Add(P, V2Mult(t2, d));
+		}
+
+		else
+		{
+			*Intersection1 = V2Add(P, V2Mult(t2, d));
+		}
+	}
+	return Result;
+}
+
+internal uint
+IntersectSegmentCircle(v2 P, v2 Dir, v2 Focus, f32 Radius, v2 *Intersection1, v2 *Intersection2)
+{
+	f32 t1, t2;
+	v2 d = Norm(Dir);
+	uint Result = IntersectLineCircleForT(P, d, Focus, Radius, &t1, &t2);
+
+	if(Result)
+	{ 
+		b32 FirstPoint = 1;
+		b32 SecondPoint = 1;
+		if(t1 < 0.f)                            FirstPoint = 0;
+		if(LenSq(V2Mult(t2, d)) > LenSq(Dir))  SecondPoint = 0;
+		Result = FirstPoint + SecondPoint;
+
+		if(FirstPoint)
+		{
+			*Intersection1 = V2Add(P, V2Mult(t1, d));
+			*Intersection2 = V2Add(P, V2Mult(t2, d));
+		}
+		else if(SecondPoint)
+		{
+			*Intersection1 = V2Add(P, V2Mult(t2, d));
+		}
+	}
+	return Result;
+}
+
+internal uint
+IntersectCircles(v2 Focus1, f32 R1, v2 Focus2, f32 R2, v2 *Intersection1, v2 *Intersection2)
+{
+	v2 Dir = V2Sub(Focus2, Focus1);
+	// TODO: optimise out
+	f32 dSq = LenSq(Dir);
+	f32 RadAdd = R1 + R2;
+	uint Result = 0;
+	if(dSq == RadAdd * RadAdd)
+	{
+		// NOTE: early out for tangents
+		Result = 1;
+		*Intersection1 = V2Add(Focus1, V2Mult(R1, Norm(Dir)));
+	}
+
+	else
+	{
+		f32 Fraction = 0.5f * ((R1*R1 / dSq) - (R2*R2 / dSq) + 1);
+		v2 ChordCross = V2Add(Focus1, V2Mult(Fraction, Dir));
+		Result = IntersectLineCircle(ChordCross, Perp(Dir), Focus1, R1, Intersection1, Intersection2);
+	}
+	return Result;
+}
+
+#define GEOMETRY_H
+#endif
