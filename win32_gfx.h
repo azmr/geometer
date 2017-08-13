07db0a54462d66679c1e1a59d6920c5cf4348047
Andrew Reece <azmreece@gmail.com>
2017-08-13 23:03:16 +0100

    include win32_gfx.h

---
+++ win32_gfx.h
@@ -0,0 +1,282 @@
+#if !defined(WIN32_GFX_H)
+typedef struct win32_image_buffer
+{
+    // NOTE: Pixels are always 32-bits wide, Memory Order BB GG RR AA
+    void *Memory;
+    int Width;
+    int Height;
+    int Pitch;
+    BITMAPINFO Info;
+} win32_image_buffer;
+
+typedef struct win32_window
+{
+	HWND Handle;
+	f32 TargetSecondsPerFrame;
+
+	int Width;
+	int Height;
+	int OffsetX;
+	int OffsetY;
+} win32_window;
+win32_window ZeroWin32Window = {0};
+
+internal inline void
+Win32ScreenResolution(HWND Handle, int *Width, int *Height)
+{
+	HDC DeviceContext = GetDC(Handle);
+	*Width = GetDeviceCaps(DeviceContext, HORZRES);
+	*Height = GetDeviceCaps(DeviceContext, VERTRES);
+	ReleaseDC(Handle, DeviceContext);
+}
+
+internal void
+Win32ResizeDIBSection(win32_image_buffer *Buffer, int Width, int Height)
+{
+	BEGIN_TIMED_BLOCK;
+	// TODO: Bulletproof
+	// maybe free after, then free first if that fails.
+
+	if(Buffer->Memory)
+	{
+		VirtualFree(Buffer->Memory, 0, MEM_RELEASE);
+		// Protects from use after free in stale memory page:
+	}
+
+	Buffer->Width = Width;
+	Buffer->Height = Height;
+
+	// NOTE: negative biHeight tells window to treat top as 0, not bottom
+	Buffer->Info.bmiHeader.biSize = sizeof(Buffer->Info.bmiHeader);
+	Buffer->Info.bmiHeader.biWidth = Width;
+	Buffer->Info.bmiHeader.biHeight = Height;
+	Buffer->Info.bmiHeader.biPlanes = 1;
+	Buffer->Info.bmiHeader.biBitCount = 32;
+	Buffer->Info.bmiHeader.biCompression = BI_RGB;
+
+	int BitmapMemorySize = Width*Height*BytesPerPixel;
+	Buffer->Memory = VirtualAlloc(0, BitmapMemorySize, MEM_RESERVE|MEM_COMMIT, PAGE_READWRITE);
+	Buffer->Pitch = Width*BytesPerPixel;
+
+	// TODO: Probably clear to black
+	END_TIMED_BLOCK;
+}
+
+
+/// ensure window info is up to date before calling
+internal b32
+Win32DisplayBufferInWindow(win32_image_buffer *Buffer, win32_window Window)
+{
+	BEGIN_TIMED_BLOCK;
+	HDC DeviceContext = GetDC(Window.Handle);
+	b32 Fullscreen = 0;
+
+#if 0
+	if((Window.Width >= Buffer->Width*2) &&
+	   (Window.Height >= Buffer->Height*2))
+	{
+		StretchDIBits(DeviceContext,
+				0, 0, 2*Buffer->Width, 2*Buffer->Height,	// Source
+				0, 0, Buffer->Width, Buffer->Height,	// Destination
+				Buffer->Memory,
+				&Buffer->Info,
+				DIB_RGB_COLORS, SRCCOPY);
+		
+		Fullscreen = 1;
+	}
+	else
+#endif
+	{
+		PatBlt(DeviceContext, 0, 0, Window.Width, Window.OffsetY, BLACKNESS);
+		PatBlt(DeviceContext, 0, Window.OffsetY + Buffer->Height, Window.Width, Window.Height, BLACKNESS);
+		PatBlt(DeviceContext, 0, 0, Window.OffsetX, Window.Height, BLACKNESS);
+		PatBlt(DeviceContext, Window.OffsetX + Buffer->Width, 0, Window.Width, Window.Height, BLACKNESS);
+
+		// NOTE: For prototyping purposes, we're going to always blit
+		// 1-to-1 pixels to make sure we don't introduce artifacts with
+		// stretching while we are learning to code the renderer!
+		StretchDIBits(DeviceContext,
+				Window.OffsetX, Window.OffsetY, Buffer->Width, Buffer->Height,	// Source
+				0, 0, Buffer->Width, Buffer->Height,	// Destination
+				Buffer->Memory,
+				&Buffer->Info,
+				DIB_RGB_COLORS, SRCCOPY);
+	}
+	ReleaseDC(Window.Handle, DeviceContext);
+	END_TIMED_BLOCK;
+	return Fullscreen;
+}
+
+internal inline void
+Win32GetWindowDimensionAndOffset(win32_window *Window, int BufferWidth, int BufferHeight)
+{
+	BEGIN_TIMED_BLOCK;
+	RECT ClientRect;
+	GetClientRect(Window->Handle, &ClientRect);
+	Window->Width = ClientRect.right - ClientRect.left;
+	Window->Height = ClientRect.bottom - ClientRect.top;
+	Window->OffsetX = (Window->Width - BufferWidth) / 2;
+	Window->OffsetY = (Window->Height - BufferHeight) / 2;
+	END_TIMED_BLOCK;
+}
+
+internal void
+Win32ToggleFullscreen(HWND Window)
+{
+	// NOTE: This follows Raymond Chen's prescription for fullscreen toggling:
+	// https://blogs.msdn.microsoft.com/oldnewthing/20100412-00/?p=14353
+
+	DWORD Style = GetWindowLong(Window, GWL_STYLE);
+	if (Style & WS_OVERLAPPEDWINDOW) {
+		MONITORINFO MonitorInfo = {sizeof(MonitorInfo)};
+		if (GetWindowPlacement(Window, &GlobalWindowPosition) &&
+			GetMonitorInfo(MonitorFromWindow(Window, MONITOR_DEFAULTTOPRIMARY), &MonitorInfo))
+		{
+			SetWindowLong(Window, GWL_STYLE, Style & ~WS_OVERLAPPEDWINDOW);
+			SetWindowPos(Window, HWND_TOP,
+						 MonitorInfo.rcMonitor.left, MonitorInfo.rcMonitor.top,
+						 MonitorInfo.rcMonitor.right - MonitorInfo.rcMonitor.left,
+						 MonitorInfo.rcMonitor.bottom - MonitorInfo.rcMonitor.top,
+						 SWP_NOOWNERZORDER | SWP_FRAMECHANGED);
+		}
+	}
+	else
+	{
+		SetWindowLong(Window, GWL_STYLE, Style | WS_OVERLAPPEDWINDOW);
+		SetWindowPlacement(Window, &GlobalWindowPosition);
+		SetWindowPos(Window, NULL, 0, 0, 0, 0,
+					 SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER |
+					 SWP_NOOWNERZORDER | SWP_FRAMECHANGED);
+	}
+}
+
+#if !defined(USING_INPUT)
+internal void
+Win32ProcessPendingMessages()
+{
+	BEGIN_TIMED_BLOCK;
+	// b32 Success
+	MSG Msg;
+	while(PeekMessage(&Msg, 0, 0, 0, PM_REMOVE))
+	{
+		switch(Msg.message)
+		{
+            case WM_QUIT:
+            {
+            	// TODO: return 0 for caller to deal with
+                GlobalRunning = 0;
+            } break;
+
+			default:
+			{
+				TranslateMessage(&Msg);
+				DispatchMessageA(&Msg);
+			} break;
+		}
+	}
+	END_TIMED_BLOCK;
+}
+#endif
+
+// TODO: Both of these???
+internal LRESULT CALLBACK
+Win32MainWindowCallback(HWND   Window,
+						UINT   Msg,
+						WPARAM WParam,
+						LPARAM LParam)
+{
+	LRESULT Result = 0;
+
+	switch(Msg)
+	{
+		case WM_CLOSE:
+		case WM_DESTROY:
+		{
+			OutputDebugStringA("Close/Destroy\n");
+			GlobalRunning = 0;
+		} break;
+		case WM_SIZE:
+		{ } break;
+
+		default:
+		{
+			// TODO: "It is more efficient to perform any move or size change processing
+			// during the WM_WINDOWPOSCHANGED message without calling DefWindowProc" [MSDN]
+			// NOTE: 88 byte leak here
+			Result = DefWindowProcA(Window, Msg, WParam, LParam);
+		} break;
+	}
+
+	return Result;
+}
+
+// TODO: not catastrophic if fails?
+internal void
+Win32SetWindowSize(HWND Handle, int W, int H, u32 Style)
+{
+	RECT Dimensions;
+	Dimensions.left = 0;
+	Dimensions.top = 0;
+	Dimensions.right = W;
+	Dimensions.bottom = H;
+	AdjustWindowRect(&Dimensions, Style, 0);
+	SetWindowPos(Handle, 0, 0, 0, Dimensions.right - Dimensions.left, Dimensions.bottom - Dimensions.top, SWP_NOMOVE);
+}
+
+// TODO: WindowClass might be useful at later date?
+internal b32
+Win32BasicWindow(HINSTANCE Instance, win32_window *Window, int W, int H, char *WindowName)
+{
+	*Window = ZeroWin32Window;
+	WNDCLASSA WindowClass = {0};
+    WindowClass.style = CS_HREDRAW|CS_VREDRAW;
+	WindowClass.lpfnWndProc = Win32MainWindowCallback;
+	WindowClass.hInstance = Instance;
+	WindowClass.hCursor = LoadCursor(0, IDC_ARROW);
+//	WindowClass.hIcon;
+	WindowClass.lpszClassName = "BasicWindowClass";
+	if(!RegisterClassA(&WindowClass))
+	{
+		return 0;
+	}
+	u32 WindowStyle = WS_OVERLAPPEDWINDOW | WS_VISIBLE;
+	Window->Handle =
+		CreateWindowExA(
+			0, //WS_EX_TOPMOST|WS_EX_LAYERED,
+			WindowClass.lpszClassName,
+			WindowName,
+			WindowStyle,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			0,
+			0,
+			Instance,
+			0);
+
+	// TODO: How do we reliably query this on Windows?
+	HDC RefreshDC = GetDC(Window->Handle);
+	// NOTE: default 60Hz refreshrate if test fails
+	int MonitorRefreshHz = 60;
+	int Win32RefreshRate = GetDeviceCaps(RefreshDC, VREFRESH);
+	ReleaseDC(Window->Handle, RefreshDC);
+
+	if(Win32RefreshRate > 1)
+	{
+		MonitorRefreshHz = Win32RefreshRate;
+	}
+#if 0 // 30FPS
+	f32 UpdateHz = (MonitorRefreshHz / 2.0f);
+#else // 60FPS
+	f32 UpdateHz = (f32)MonitorRefreshHz;
+#endif
+	Window->TargetSecondsPerFrame = (1.0f / (f32)UpdateHz);
+
+	Win32SetWindowSize(Window->Handle, W, H, WindowStyle);
+
+	return 1;
+}
+
+#define WIN32_GFX_H
+#endif
